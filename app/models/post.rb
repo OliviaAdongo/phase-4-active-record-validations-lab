9c64b5d273d165a7bea08bbd90b437e4841504ac
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-03 11:19:12 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validate :title_is_clickbait
+    validates :category ,inclusion: {in: ['Fiction','Non-Fiction']}
+
+
+    # CUSTOM VALIDATIONS 
+    def title_is_clickbait
+        if title == "True Facts"
+            errors.add(:title,  "This is clickbait")
+        end
+    end
 end
+ 
